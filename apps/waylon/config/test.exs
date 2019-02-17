36c442f825f986453720e7386f10a9ff7d79fd27
Thyago Stall <thstall@gmail.com>
2019-02-17 12:48:51 -0300

    Refactors dev & test config files

--- apps/waylon/config/test.exs
+++ apps/waylon/config/test.exs
@@ -1,13 +1,3 @@
-# Since configuration is shared in umbrella projects, this file
-# should only configure the :waylon application itself
-# and only for organization purposes. All other config goes to
-# the umbrella root.
 use Mix.Config
 
-# Configure your database
-config :waylon, Waylon.Repo,
-  username: "root",
-  password: "",
-  database: "waylon_test",
-  hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox
+import_config "test.secret.exs"
